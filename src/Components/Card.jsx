41fbe891523c7bdf340ad514abe74ff98a3e0915
Nicolas Duarte <nickboytube0418@gmail.com>
2025-04-15 11:01:40 -0300

    ...

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,9 +5,7 @@ import estilos from './Card.module.css';
 export function Card({movie, onOpenModal}) {
     return (
         <div className={estilos.container}>
-            <h3>{movie.title}</h3>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} onClick={() => onOpenModal(movie)} />
-            <p>{movie.overview}</p>
         </div>
     );
 }
